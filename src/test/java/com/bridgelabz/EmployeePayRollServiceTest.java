cc1b6c9cae928325f07063c575124b89713b7452
SW-GAGAN <srgaganreddy0@gmail.com>
2021-08-13 00:12:14 +0530

    Adding; Counting Entries and Counting

--- src/test/java/com/bridgelabz/EmployeePayRollServiceTest.java
+++ src/test/java/com/bridgelabz/EmployeePayRollServiceTest.java
@@ -10,13 +10,15 @@ public class EmployeePayRollServiceTest {
     public void given3Employees_WhenWriitenToFile_ShouldMatchEmployeeEntries() {
         EmployeePayrollData[] arrayOfEmployees = {
                 new EmployeePayrollData(1, "Gagan Reddy", 700000.00),
-                new EmployeePayrollData(2, "Shravya Reddy", 400000.00),
+                new EmployeePayrollData(2, "Shravya Reddy", 500000.00),
                 new EmployeePayrollData(3, "Dilip Gowda", 300000.00)
         };
         EmployeePayrollService employeePayrollService = new EmployeePayrollService(Arrays.asList(arrayOfEmployees));
         employeePayrollService.writingData(EmployeePayrollService.IOService.FILE_IO);
-
+        employeePayrollService.printData(EmployeePayrollService.IOService.FILE_IO);
         long entries = employeePayrollService.countEntries(EmployeePayrollService.IOService.FILE_IO);
+        System.out.println("No.of entries into file are: " + entries);
+
         Assertions.assertEquals(3, entries);
     }
 }
